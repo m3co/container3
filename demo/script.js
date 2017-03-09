08be2ddb4cf2c57c2933b279c6ab7fe1df21c5d9
Daniyal Gabitov <danuy@list.ru>
2017-03-09 15:16:30 +0300

    [DEMO] Attempt to integrate with fragment

--- demo/script.js
+++ demo/script.js
@@ -1,11 +1,13 @@
 'use strict';
 
 (() => {
-  let edit = document.querySelector('[data-container3-edit]');
-  let container = document.querySelector('[data-container3]');
-  let form = document.querySelector('[data-container3-form]');
+  let cf = document.currentFragment;
+  let edit = cf.querySelector('[data-container3-edit]');
+  let container = cf.querySelector('[data-container3]');
+  let form = cf.querySelector('[data-container3-form]');
   let textarea = form.querySelector('textarea');
   let originalHTML = container.innerHTML.trim();
+
   edit.addEventListener('click', () => {
     edit.hidden = true;
     form.hidden = false;
@@ -20,16 +22,16 @@
     container.hidden = false;
     container.innerHTML = textarea.value;
     originalHTML = container.innerHTML.trim();
-    [...container.querySelectorAll('script')].forEach((script) => {
-      console.log(script);
-      let oldScript = script;
-      let newScript = document.createElement('script');
-
-      [...oldScript.attributes]
-        .forEach(attr => newScript.setAttribute(attr.name, attr.value));
-      newScript.text = oldScript.text;
-      oldScript.parentNode.replaceChild(newScript, oldScript);
-    });
+    // [...container.querySelectorAll('script')].forEach((script) => {
+    //   console.log(script);
+    //   let oldScript = script;
+    //   let newScript = document.createElement('script');
+    //
+    //   [...oldScript.attributes]
+    //     .forEach(attr => newScript.setAttribute(attr.name, attr.value));
+    //   newScript.text = oldScript.text;
+    //   oldScript.parentNode.replaceChild(newScript, oldScript);
+    // });
   });
 
 })();
