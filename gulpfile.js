5038dee2909adeabe46a820403ed8c715eedfe84
Daniyal Gabitov <danuy@list.ru>
2017-03-10 12:34:27 +0300

    Add styles, change folders structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,134 @@
-./resources/configs/gulpfile.js
+'use strict';
+
+const gulp         = require('gulp');
+const csslint      = require('gulp-csslint');
+const htmlhint     = require("gulp-htmlhint");
+const eslint       = require('gulp-eslint');
+const babel        = require('gulp-babel');
+const connect      = require('gulp-connect');
+const jsdoc        = require('gulp-jsdoc3');
+const gulpIf       = require('gulp-if');
+const argv         = require('yargs').argv;
+const fileinclude  = require('gulp-file-include');
+const sass         = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+const paths = {
+  src:   './src',
+  dst:   './build',
+  css:   ['./src/**/*.s{c,a}ss'],
+  js:    ['./src/**/*.js'],
+  html:  ['./src/**/*.html'],
+  fonts: ['./src/**/*.{eot,ttf,woff,woff2}'],
+  json:  ['./src/**/*.json']
+};
+
+const port = (argv.port === undefined) ? 8080 : argv.port;
+
+function isFixed(file) {
+  // Has ESLint fixed the file contents?
+  return file.eslint != null && file.eslint.fixed;
+}
+
+gulp.task('reload', function () {
+  return gulp.src(paths.src, { read: false })
+    .pipe(connect.reload());
+});
+
+gulp.task('doc', () => {
+  // var config = require('./.jsdoc.json');
+  // return gulp.src(['./README.md'].concat(paths.js))
+  //   .pipe(jsdoc(config));
+});
+
+gulp.task('css-lint', () => {
+  // return gulp.src(paths.css)
+  //   .pipe(csslint())
+  //   .pipe(csslint.formatter());
+});
+
+gulp.task('html-hint', () => {
+  return gulp.src(paths.html)
+    .pipe(htmlhint())
+    .pipe(htmlhint.reporter());
+});
+
+gulp.task('js-lint', () => {
+  return gulp.src(paths.js, { base: './' })
+    .pipe(fileinclude({
+      prefix: '@@',
+      indent: true
+    }))
+    .pipe(eslint({fix: true}))
+    .pipe(eslint.format())
+    .pipe(gulpIf(isFixed, gulp.dest('./')));
+});
+
+gulp.task('js-copy', () => {
+  return gulp.src(paths.js)
+    .pipe(fileinclude({
+      prefix: '@@',
+      indent: true
+    }))
+    .pipe(babel())
+    .pipe(gulp.dest(paths.dst));
+});
+
+gulp.task('css-copy', () => {
+  // return gulp.src(paths.css)
+  //   .pipe(gulp.dest(paths.dst));
+});
+
+gulp.task('html-copy', () => {
+  return gulp.src(paths.html)
+    .pipe(gulp.dest(paths.dst));
+});
+
+gulp.task('fonts-copy', () => {
+  return gulp.src(paths.fonts)
+    .pipe(gulp.dest(paths.dst));
+});
+
+gulp.task('json-copy', () => {
+  return gulp.src(paths.json)
+    .pipe(gulp.dest(paths.dst));
+});
+
+gulp.task('sass', function () {
+  return gulp.src(paths.css)
+    .pipe(sass({includePaths: ['node_modules/material-design-lite/src', 'node_modules/foundation-sites/scss']})
+    .on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(paths.dst));
+});
+
+gulp.task('watch', () => {
+  gulp.watch(paths.html, ['html-hint', 'html-copy', 'doc', 'reload']);
+  gulp.watch(paths.js, ['js-lint', 'js-copy', 'doc', 'reload']);
+  gulp.watch(paths.css, ['css-lint', 'sass', 'reload']);
+  gulp.watch(paths.fonts, ['fonts-copy', 'reload']);
+  gulp.watch(paths.json, ['json-copy', 'reload']);
+});
+
+gulp.task('connect', () => {
+  connect.server({
+    root: paths.dst,
+    livereload: true,
+    port: port
+  });
+});
+
+gulp.task('default', [
+  'js-lint',
+  'css-lint',
+  'html-hint',
+  'css-copy',
+  'fonts-copy',
+  'html-copy',
+  'sass',
+  'js-copy',
+  'json-copy',
+  'doc',
+  'watch',
+  'connect'
+]);
---
+++ src/demo/fragments/table3/script.js
@@ -0,0 +1,88 @@
+'use strict';
+
+var table = d3.select('#example-table');
+fetch('data.json').then(function (response) {
+  return response.json();
+}).then(function (json) {
+  var data = json.data;
+  var columns = Object.keys(data[0] ? data[0] : {}).map(function (c) {
+    return {
+      sort: null,
+      text: c,
+      key: c
+    };
+  });
+
+  // filtering
+  table.select('#filter').on('keyup', function () {
+    var value = d3.event.target.value;
+    table.select('tbody').selectAll('tr').each(function (d, i, trs) {
+      var tr = trs[i];
+      if (value === '') {
+        tr.hidden = false;
+      } else if (tr.textContent.indexOf(value) === -1) {
+        tr.hidden = true;
+      }
+    });
+  });
+
+  // head & sorting
+  table.select('thead tr').selectAll('th').data(columns).enter().append('th').text(function (d) {
+    return d.text;
+  }).on('click', function (d, i, ths) {
+    if (!d.sort) {
+      d.sort = 'asc';
+      ths[i].classList.add('th--sort-asc');
+    } else if (d.sort == 'asc') {
+      d.sort = 'desc';
+      ths[i].classList.add('th--sort-desc');
+    } else if (d.sort == 'desc') {
+      d.sort = null;
+      ths[i].classList.remove('th--sort-asc');
+      ths[i].classList.remove('th--sort-desc');
+    }
+
+    var isNumber = function isNumber(n) {
+      return isFinite(n) && +n === n;
+    };
+    var prepare = function prepare(d) {
+      return isNumber(d) ? Number(d) : !d ? '' : d;
+    };
+
+    renderBody(data.map(function (d) {
+      return d;
+    }).sort(function (a, b) {
+      return columns.reduce(function (r, d) {
+        if (r !== 0) {
+          return r;
+        }
+        if (d.sort == null) {
+          return 0;
+        }
+        var a_ = prepare(a[d.key]); // this may be in other place
+        var b_ = prepare(b[d.key]); // this may be in other place
+        if (d.sort === 'asc') {
+          return d3.ascending(a_, b_);
+        } else if (d.sort === 'desc') {
+          return d3.descending(a_, b_);
+        }
+      }, 0);
+    }));
+  });
+
+  renderBody(data);
+
+  function renderBody(data) {
+    var tr = table.select('tbody').selectAll('tr').data(data);
+
+    var td = tr.enter().append('tr').merge(tr).selectAll('td').data(function (d) {
+      return columns.map(function (c) {
+        return d[c.key];
+      });
+    });
+
+    td.enter().append('td').merge(td).text(function (d) {
+      return d;
+    });
+  }
+});
