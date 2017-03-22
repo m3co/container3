a181e4e0bba52ab932d7470ebe7ba6bd9fc9e396
Andres Rios <rianby64@gmail.com>
2017-03-22 16:43:00 +0300

    Add a template with a minimal set of features

--- src/container.js
+++ src/container.js
@@ -1,8 +1,45 @@
 (!window.HTMLXContainerElement) && (() => {
 
+  // all this is just for importing the code without using fragments.
+  // The imported code lays on an absolute path
+  // the path "/container.html" is a temporary solution. It must be relative
+  var template = fetch('/container.html')
+    .then(response => {
+      if (response.status >= 200 && response.status < 300) {
+        return response.text();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(text => {
+      var template = document.createDocumentFragment();
+      var content = document.createElement('div');
+      content.innerHTML = text;
+      [...content.querySelectorAll('script')].forEach((script) => {
+        let newScript = document.createElement('script');
+
+        [...script.attributes]
+          .forEach(attr => newScript.setAttribute(attr.name, attr.value));
+        newScript.text = script.text;
+        script.parentNode.replaceChild(newScript, script);
+      });
+      while(content.firstChild) {
+        template.appendChild(content.firstChild);
+      }
+      return template;
+    });
+
   class HTMLXContainerElement extends HTMLElement {
     constructor() {
       super();
+      this.templateReady = new Promise((resolve, reject) => {
+        template.catch(reason => {
+          reject(reason);
+        }).then((template) => {
+          var clone = document.importNode(template, true);
+          this.appendChild(clone);
+          resolve(this);
+        });
+      });
     }
   }
 
--- test/index.js
+++ test/index.js
@@ -1,4 +1,19 @@
 'use strict';
 test(() => {
   assert_true(HTMLXContainerElement.prototype instanceof HTMLElement);
-}, "HTMLXContainerElement is defined");
+}, "Container is defined as HTMLXContainerElement");
+
+test(() => {
+  var container = document.createElement('x-container');
+  assert_true(container.templateReady instanceof Promise,
+    "templateReady is defined");
+
+  promise_test(function() {
+    var container = document.createElement('x-container');
+    return container.templateReady.then(this.step_func((container) => {
+      assert_true(container.querySelector('[edit=""]') instanceof HTMLElement);
+      assert_true(container.querySelector('[content=""]') instanceof HTMLElement);
+      assert_true(container.querySelector('[form=""]') instanceof HTMLElement);
+    }));
+  }, "The container holds edit, content, form elements");
+}, "Hold a template with a clear set of features");
